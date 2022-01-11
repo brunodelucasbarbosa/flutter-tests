19030f9a0e6a8535c615ca4840592ceec1321ddc
Bruno Barbosa <brunnobarbosas@gmail.com>
2022-01-10 23:18:20 -0300

    variaveis

---
+++ logica-dart/hello.dart
@@ -0,0 +1,12 @@
+void main() {
+  var nome = 'Bruno';
+  if (nome == 'Bruno') {
+    print('Hello ${nome}');
+  } else {
+    print('Not me');
+  }
+
+  var age = 22;
+
+  print('Hello ${nome}, my agee is ${age}');
+}
